8819c6c179b8d11c3e416d60bc5b8009698b8214
Tisha Kadecha <72653803+tishakadecha@users.noreply.github.com>
2023-09-05 20:17:55 +0530

    Python Basic Programs

---
+++ Python_PGs/Modules/mod2.py
@@ -0,0 +1,9 @@
+import mod1 as m
+a = m.sum(10,20)
+b = m.sub(100,20)
+c = m.mul(25,2)
+d = m.div(100,10)
+print("Total = ",a)
+print("Substraction = ",b)
+print("Multiplication = ",c)
+print("Division = ",d)
---
+++ Python_PGs/Modules/swap2.py
@@ -0,0 +1,9 @@
+import swap as s
+a = 11
+b = 15
+print("Before Swapping ")
+print("1st Value = ",a)
+print("2nd Value = ",b)
+print("After Swapping ")
+s.swapnum(a,b)
+
